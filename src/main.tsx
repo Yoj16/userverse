e1b4140a6503f3dca00d303194d71fc568eabca9
Joy <joy.heu@outlook.fr>
2023-09-29 15:37:45 +0200

    build(sass): install sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './main.scss';
 import { Home } from './pages/Home';
 import { UserFlex } from './pages/UserFlex';
 import { UserGrid } from './pages/UserGrid';
