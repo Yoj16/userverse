75dd987703d42443f3ba6a02e4b6819aacf032db
Joy <joy.heu@outlook.fr>
2023-10-02 09:29:41 +0200

    feat: improve style card-component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,8 +10,10 @@ type CardProps = {
 export const Card: FC<CardProps> = ({ id, title, picture, description }) => {
     return (
         <div key={id} className='container'>
-            <img src={picture} alt={picture} className='card-image'/>
-            <div>
+            <div className='image-block'>
+                <img src={picture} alt={picture} className='card-image'/>
+            </div>
+            <div className='text-block'>
                 <h1>{title}</h1>
                 <p>{description}</p>
             </div>
