92b7a672015f09edde5929dc4eb60e26d94f3146
Joy <joy.heu@outlook.fr>
2023-10-02 09:49:41 +0200

    feat: remove fake data unuseful

--- src/pages/UserFlex.tsx
+++ src/pages/UserFlex.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Card from "../components/card/Card";
 
 export const UserFlex = (): JSX.Element => (
     <header>
         <h1>This is the UserFlex Page.</h1>
-        <Card 
-            id={2}
-            picture="img.png"
-            title="Joy"
-            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        />
     </header>
 )
